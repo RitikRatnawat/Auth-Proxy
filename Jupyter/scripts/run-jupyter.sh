9a18ea58d1bf8399ad9e2ee584b7ede705b8ae36
RitikRatnawat <ritikratnawat786@gmail.com>
2025-04-10 21:19:47 +0530

    Changed from Notebook Environment to Lab Environment

--- Jupyter/scripts/run-jupyter.sh
+++ Jupyter/scripts/run-jupyter.sh
@@ -3,4 +3,4 @@
 echo "Running Jupyter Server"
 export JUPYTER_SERVER_IP=`hostname -i`
 
-jupyter notebook --config=/app/config/jupyter_server_config.py --ip=$JUPYTER_SERVER_IP --allow-root
+jupyter lab --config=/app/config/jupyter_server_config.py --ip=$JUPYTER_SERVER_IP --allow-root
