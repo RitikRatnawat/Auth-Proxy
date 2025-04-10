85995be16e36b8a896118e81c95c4c156b73878d
RitikRatnawat <ritikratnawat786@gmail.com>
2025-04-10 19:55:52 +0530

    Updated Entrypoint Script

--- Jupyter/scripts/run_jupyter.sh
+++ Jupyter/scripts/run_jupyter.sh
@@ -1,5 +1,5 @@
 echo "Running Jupyter Server"
 export JUPYTER_SERVER_IP=`hostname -i`
 
-python -m notebook --config=/app/config/jupyter_server_config.py \
+jupyter notebook --config=/app/config/jupyter_server_config.py \
                    --ip=$JUPYTER_SERVER_IP
