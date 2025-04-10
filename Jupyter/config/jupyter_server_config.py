fce3ffe5c67585ff52661520eb6e8b43c4dfb614
RitikRatnawat <ritikratnawat786@gmail.com>
2025-04-10 19:28:21 +0530

    Create Jupyter Server Configuration

---
+++ Jupyter/config/jupyter_server_config.py
@@ -0,0 +1,7 @@
+# Configuration for the Jupyter Server
+
+c.ServerApp.ip = "0.0.0.0"
+c.ServerApp.root_dir = "/shared/repos"
+c.ServerApp.port = 8080
+c.ServerApp.allow_origin = "*"
+c.IdentityProvider.token = ""
