00f72d6bbd43083c3d72244af7d5390b4408883e
RitikRatnawat <ritikratnawat786@gmail.com>
2025-04-10 20:57:27 +0530

    Renamed Entrypoint Script

---
+++ Jupyter/scripts/run-jupyter.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Running Jupyter Server"
+export JUPYTER_SERVER_IP=`hostname -i`
+
+jupyter notebook --config=/app/config/jupyter_server_config.py --ip=$JUPYTER_SERVER_IP --allow-root
--- Jupyter/scripts/run_jupyter.sh
+++
@@ -1,5 +0,0 @@
-echo "Running Jupyter Server"
-export JUPYTER_SERVER_IP=`hostname -i`
-
-jupyter notebook --config=/app/config/jupyter_server_config.py \
-                   --ip=$JUPYTER_SERVER_IP
